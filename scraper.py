2eae0a73b5fcabf335d35a022957c7aca71ea13d
Fawad Ali <fawad.ali@outlook.com>
2026-05-11 18:50:47 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -142,7 +142,7 @@ if DEBUG_ON:
 # ────────────────────────────────────────────────────────────────…[...]
 
 if TESTING_ON:
-    TARGET_DATE = datetime(2026, 5, 11)
+    TARGET_DATE = datetime(2026, 5, 9)
 else:
     DATE_OVERRIDE = os.environ.get("DATE_OVERRIDE")
     if DATE_OVERRIDE:
